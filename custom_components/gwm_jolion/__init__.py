5f8c4ec5215a0ab657c040f3f5f46660b93bd5d6
IndeecDen <60156503+IndeecDen@users.noreply.github.com>
2026-09-06 19:27:12 +0300

    Load primary card editor before card

--- custom_components/gwm_jolion/__init__.py
+++ custom_components/gwm_jolion/__init__.py
@@ -29,19 +29,19 @@ _LOGGER = logging.getLogger(__name__)
 FRONTEND_DIR = Path(__file__).parent / "frontend"
 FRONTEND_ASSETS = (
     (
-        FRONTEND_DIR / "gwm-jolion-card.js",
-        "/gwm-jolion/gwm-jolion-card.js",
-        "/gwm-jolion/gwm-jolion-card.js?v=0.1.0-alpha.16",
-    ),
-    (
         FRONTEND_DIR / "gwm-jolion-card-editor.js",
         "/gwm-jolion/gwm-jolion-card-editor.js",
-        "/gwm-jolion/gwm-jolion-card-editor.js?v=0.1.0-alpha.16",
+        "/gwm-jolion/gwm-jolion-card-editor.js?v=0.1.0-alpha.17",
+    ),
+    (
+        FRONTEND_DIR / "gwm-jolion-card.js",
+        "/gwm-jolion/gwm-jolion-card.js",
+        "/gwm-jolion/gwm-jolion-card.js?v=0.1.0-alpha.17",
     ),
     (
         FRONTEND_DIR / "gwm-jolion-remote-card.js",
         "/gwm-jolion/gwm-jolion-remote-card.js",
-        "/gwm-jolion/gwm-jolion-remote-card.js?v=0.1.0-alpha.16",
+        "/gwm-jolion/gwm-jolion-remote-card.js?v=0.1.0-alpha.17",
     ),
 )
 DATA_FRONTEND_REGISTERED = "_frontend_registered"
